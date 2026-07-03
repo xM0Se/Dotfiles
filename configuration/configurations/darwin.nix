91bf15c5ddaa48584d94848159cc8c4a8ab4c288
xm0se <189546389+xM0Se@users.noreply.github.com>
2026-07-03 18:53:43 +0200

    feat(configuration/configuration): added system data removal tool to darwin configuration

--- configuration/configurations/darwin.nix
+++ configuration/configurations/darwin.nix
@@ -1,10 +1,12 @@
 _: {
   imports = [
     ../modules/darwin/dock.nix
+    ../modules/darwin/system-data.nix
     ../modules/darwin/finder.nix
     ../modules/darwin/hotcorners.nix
     ../modules/darwin/sops.nix
     ../modules/darwin/home-manager.nix
     ../modules/common/common.nix
   ];
+  system-data.enable = true;
 }
