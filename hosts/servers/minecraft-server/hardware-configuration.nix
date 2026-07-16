2d42c4ca836a583956c76a40a8a6e241b61bd9fb
xm0se <189546389+xM0Se@users.noreply.github.com>
2026-07-16 17:38:57 +0200

    chore(cleanup)

--- hosts/servers/minecraft-server/hardware-configuration.nix
+++ hosts/servers/minecraft-server/hardware-configuration.nix
@@ -12,23 +12,7 @@
     };
     kernelModules = [];
     extraModulePackages = [];
-    # loader.grub = {
-    #   enable = true;
-    #   device = "/dev/sda";
-    # };
   };
-
-  # fileSystems = {
-  #   "/" = {
-  #     device = "/dev/disk/by-label/NIXROOT";
-  #     fsType = "ext4";
-  #   };
-  #   "/boot" = {
-  #     device = "/dev/disk/by-label/NIXBOOT";
-  #     fsType = "vfat";
-  #     options = ["fmask=0022" "dmask=0022"];
-  #   };
-  # };
 
   swapDevices = [];
 
