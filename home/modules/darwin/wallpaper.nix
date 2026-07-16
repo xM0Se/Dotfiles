a3be7be2ced746c6a409d8bb710bd58cd12c13cc
xm0se <189546389+xM0Se@users.noreply.github.com>
2026-07-16 11:39:28 +0200

    chore(home/modules/darwin): cleaned up wallpaper and widget .nix

--- home/modules/darwin/wallpaper.nix
+++ home/modules/darwin/wallpaper.nix
@@ -9,18 +9,8 @@
     sha256 = "sha256-MwvRU7U4tO6F1duxBrHLOd7F5Gnzv/zyiZkm5EFqkY4=";
   };
 in {
-  # I can use the command ```set-wallpaper-script``` to change my wallpaper to this one
-  home.packages = [
-    (pkgs.writeShellScriptBin "set-wallpaper-script" ''
-      set -e
-      /usr/bin/osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"${wallpaper}\""
-    '')
-  ];
-  # This changes my wallpaper every rebuild
-  home.activation = {
-    setWallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      echo "Setting macOS wallpaper from Nix store..."
-      /usr/bin/osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"${wallpaper}\""
-    '';
-  };
+  home.activation.setWallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    echo "Setting macOS wallpaper from Nix store..."
+    /usr/bin/osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"${wallpaper}\""
+  '';
 }
