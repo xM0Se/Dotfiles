4c8349461bc4991ae4e3dab20241712b3edba5bc
xM0Se <git@xM0Se.dev>
2026-07-20 18:52:03 +0200

    feat(home/modules): fixing switched config of sops.nix in home-manager

--- home/modules/darwin/sops.nix
+++ home/modules/darwin/sops.nix
@@ -1,12 +1,13 @@
 {
   inputs,
   self,
+  config,
   ...
 }: {
   imports = [inputs.sops-nix.homeManagerModules.sops];
   sops = {
     defaultSopsFile = "${self}/secrets/secrets.yaml";
     defaultSopsFormat = "yaml";
-    age.keyFile = "/var/lib/sops-nix/key.txt";
+    age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
   };
 }
--- home/modules/nixos/sops.nix
+++ home/modules/nixos/sops.nix
@@ -1,13 +1,12 @@
 {
   inputs,
   self,
-  config,
   ...
 }: {
   imports = [inputs.sops-nix.homeManagerModules.sops];
   sops = {
     defaultSopsFile = "${self}/secrets/secrets.yaml";
     defaultSopsFormat = "yaml";
-    age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+    age.keyFile = "/var/lib/sops-nix/key.txt";
   };
 }
