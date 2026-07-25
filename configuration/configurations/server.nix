7bc0c99a0cfa25da924eede11f28aeb097573fe4
xM0Se <git@xM0Se.dev>
2026-07-25 11:58:18 +0200

    feat(configuration): converted custom ssh module into an option

--- configuration/configurations/server.nix
+++ configuration/configurations/server.nix
@@ -1,10 +1,12 @@
 {lib, ...}: {
   imports = [
-    ../modules/nixos/ssh/default.nix
+    ../modules/nixos/ssh
     ../modules/nixos/sops.nix
     ../modules/nixos/home-manager.nix
     ../modules/common/common.nix
   ];
+  custom.ssh.enable =
+    lib.mkDefault true;
   custom.sops.enable =
     lib.mkDefault true;
 }
--- configuration/modules/nixos/ssh/default.nix
+++ configuration/modules/nixos/ssh/default.nix
@@ -1,16 +1,27 @@
-_: {
-  services = {
-    openssh = {
-      enable = true;
-      openFirewall = true;
-      settings = {
-        UseDns = false;
-        PasswordAuthentication = false;
-        PermitRootLogin = "no";
+{
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    custom.ssh.enable =
+      lib.mkEnableOption "ssh";
+  };
+
+  config = lib.mkIf config.custom.ssh.enable {
+    services = {
+      openssh = {
+        enable = true;
+        openFirewall = true;
+        settings = {
+          UseDns = false;
+          PasswordAuthentication = false;
+          PermitRootLogin = "no";
+        };
       };
-    };
-    fail2ban = {
-      enable = true;
+      fail2ban = {
+        enable = true;
+      };
     };
   };
 }
