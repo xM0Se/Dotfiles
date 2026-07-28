42e6fd7fa514d60b677c4b4d33d0dd61f99052f2
xM0Se <git@xM0Se.dev>
2026-07-28 18:58:58 +0200

    feat(home/modules): merged darwin and nixos sops module into one

---
+++ home/modules/common/sops/default.nix
@@ -0,0 +1,28 @@
+{
+  self,
+  inputs,
+  lib,
+  pkgs,
+  config,
+  ...
+}: {
+  imports = [
+    inputs.sops-nix.homeManagerModules.sops
+  ];
+
+  options = {
+    sops.enable =
+      lib.mkEnableOption "sops";
+  };
+
+  config = lib.mkIf config.sops.enable {
+    sops = {
+      defaultSopsFile = "${self}/secrets/secrets.yaml";
+      defaultSopsFormat = "yaml";
+      age.keyFile =
+        if pkgs.stdenv.isDarwin
+        then "${config.home.homeDirectory}/.config/sops/age/keys.txt"
+        else "/var/lib/sops-nix/key.txt";
+    };
+  };
+}
