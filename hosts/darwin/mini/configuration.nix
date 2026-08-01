5f536c2d6e321e7687864d3b2349f778271a1724
xM0Se <git@xM0Se.dev>
2026-08-01 15:37:18 +0200

    chore(hosts/darwin): removed unnecessary functions

--- hosts/darwin/mini/configuration.nix
+++ hosts/darwin/mini/configuration.nix
@@ -1,17 +1,8 @@
-{
-  inputs,
-  self,
-  pkgs,
-  ...
-}: {
+{self, ...}: {
   imports = [
-    inputs.determinate.darwinModules.default
-    inputs.mac-app-util.darwinModules.default
     (self + "/pkgs/homebrew")
     (self + "/configuration/configurations/darwin.nix")
   ];
-  determinateNix.enable = true;
-  documentation.enable = false;
 
   brew = {
     mas.common.enable = false;
@@ -26,53 +17,6 @@
 
   users.users.xm0se.home = "/Users/xm0se";
 
-  environment.systemPackages = [
-    inputs.colmena.packages.${pkgs.stdenv.hostPlatform.system}.colmena
-    #--
-    pkgs.mas
-    pkgs.age
-    pkgs.sops
-    #CLI tools
-    pkgs.browsers
-    pkgs.fastfetch
-    pkgs.gh
-    pkgs.cmatrix
-    pkgs.nmap
-    pkgs.dwt1-shell-color-scripts
-    pkgs.ripgrep
-    pkgs.whatsapp-for-mac
-    pkgs.radicle-node # Includes `rad`, `radicle-node`, and related tools
-    pkgs.radicle-httpd # HTTP API for web interfaces
-    pkgs.radicle-explorer # Web frontend
-    pkgs.radicle-tui # Terminal UI
-    pkgs.radicle-desktop # Desktop app
-    (pkgs.radicle-ci-broker.overrideAttrs (_: {
-      # Radicle CI/CD
-      doCheck = false;
-    }))
-    pkgs.tldr
-    pkgs.tree
-    pkgs.bitwarden-cli
-    pkgs.obsidian
-    pkgs.keycastr
-
-    pkgs.gnupg
-    pkgs.pinentry_mac
-    pkgs.pcsc-tools
-  ];
-
-  networking = {
-    applicationFirewall = {
-      enable = true;
-      enableStealthMode = true;
-    };
-  };
-
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
   system = {
     primaryUser = "xm0se";
     defaults = {
@@ -82,31 +26,22 @@
         DisableConsoleAccess = true;
       };
 
+      screensaver = {
+        askForPassword = true;
+        askForPasswordDelay = 0;
+      };
+
       NSGlobalDomain = {
         AppleInterfaceStyle = "Dark";
         AppleIconAppearanceTheme = "RegularDark";
         AppleInterfaceStyleSwitchesAutomatically = false;
         AppleMeasurementUnits = "Centimeters";
         AppleMetricUnits = 1;
-        ApplePressAndHoldEnabled = false;
         AppleTemperatureUnit = "Celsius";
-        NSAutomaticCapitalizationEnabled = false;
-        NSAutomaticDashSubstitutionEnabled = false;
-        NSAutomaticInlinePredictionEnabled = false;
-        NSAutomaticPeriodSubstitutionEnabled = false;
-        NSAutomaticQuoteSubstitutionEnabled = false;
-        NSAutomaticSpellingCorrectionEnabled = false;
         NSAutomaticWindowAnimationsEnabled = false;
         NSWindowShouldDragOnGesture = false;
-        "com.apple.keyboard.fnState" = true;
-
         _HIHideMenuBar = true;
       };
-      screencapture = {
-        target = "clipboard";
-        type = "jpg";
-      };
-      universalaccess.reduceMotion = true;
 
       WindowManager = {
         EnableTilingByEdgeDrag = false;
@@ -114,17 +49,12 @@
         EnableTopTilingByEdgeDrag = false;
         GloballyEnabled = false;
       };
-      SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
 
-      screensaver = {
-        askForPassword = true;
-        askForPasswordDelay = 0;
-      };
+      universalaccess.reduceMotion = true;
+      SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
     };
 
     configurationRevision = self.rev or self.dirtyRev or null;
     stateVersion = 6;
   };
-
-  nixpkgs.hostPlatform = "aarch64-darwin";
 }
