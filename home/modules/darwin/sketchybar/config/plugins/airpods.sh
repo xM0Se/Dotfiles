3b30c1ed0fc3f24669135b5a8f328646bde6b9bd
xM0Se <git@xM0Se.dev>
2026-08-05 16:08:26 +0200

    feat(home/modules/darwin): fixed sketchybar airpods module

--- home/modules/darwin/sketchybar/config/plugins/airpods.sh
+++ home/modules/darwin/sketchybar/config/plugins/airpods.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source "$CONFIG_DIR/colors.sh"
 
-CASE=$(/usr/sbin/system_profiler SPBluetoothDataType | /run/current-system/sw/bin/rg -A6 "Moritz’s AirPods:" | tail -n +5 | head -n 1 | /run/current-system/sw/bin/rg -o '\d+')
+CASE=$(/usr/sbin/system_profiler SPBluetoothDataType | /usr/bin/grep -A6 "Moritz’s AirPods:" | tail -n +5 | head -n 1 | /usr/bin/grep -oE '[0-9]+')
 
-LEFT=$(/usr/sbin/system_profiler SPBluetoothDataType | /run/current-system/sw/bin/rg -A6 "Moritz’s AirPods:" | tail -n +6 | head -n 1 | /run/current-system/sw/bin/rg -o '\d+')
+LEFT=$(/usr/sbin/system_profiler SPBluetoothDataType | /usr/bin/grep -A6 "Moritz’s AirPods:" | tail -n +6 | head -n 1 | /usr/bin/grep -oE '[0-9]+')
 
-RIGHT=$(/usr/sbin/system_profiler SPBluetoothDataType | /run/current-system/sw/bin/rg -A6 "Moritz’s AirPods:" | tail -n +7 | head -n 1 | /run/current-system/sw/bin/rg -o '\d+')
+RIGHT=$(/usr/sbin/system_profiler SPBluetoothDataType | /usr/bin/grep -A6 "Moritz’s AirPods:" | tail -n +7 | head -n 1 | /usr/bin/grep -oE '[0-9]+')
 
 if [ "$CASE" -lt 10 ]; then
   sketchybar -m --set casep label="${CASE}%" label.color="$BASE09"
