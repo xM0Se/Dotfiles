58e3d1678efc19af9c61c6ba21bbf41d80d69550
xM0Se <git@xM0Se.dev>
2026-08-11 18:22:52 +0200

    feat(home/modules/common): updated vesktop theme config

--- home/modules/common/vesktop/default.nix
+++ home/modules/common/vesktop/default.nix
@@ -1,11 +1,22 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  themeRepo = pkgs.fetchFromGitHub {
+    owner = "rose-pine";
+    repo = "discord";
+    rev = "HEAD";
+    sha256 = "sha256-pwiFCJClOl5IvnbnT/bZtshK3nEAq7bdXtEdnHcTFIk=";
+  };
+  themePath = "dist/rose-pine-moon.css";
+  theme = builtins.readFile "${themeRepo}/${themePath}";
+in {
   programs.vesktop = {
     enable = true;
     package = pkgs.vesktop;
     vencord = {
-      themes = {rose-pine-moon = ./rose-pine-moon.css;};
+      themes = {
+        theme = "${theme}";
+      };
       settings = {
-        enabledThemes = ["rose-pine-moon.css"];
+        enabledThemes = ["theme.css"];
       };
     };
   };
