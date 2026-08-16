ef4d4e0989c0ac1f14ea577b5f504d4623f8ad98
xM0Se <git@xM0Se.dev>
2026-08-16 11:45:45 +0200

    feat(home/configurations): added nixos support

--- home/configurations/nixos.nix
+++ home/configurations/nixos.nix
@@ -1,1 +1,39 @@
-{}
+{
+  self,
+  inputs,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./../modules/common/vesktop
+    ./../modules/common/zsh
+    ./../modules/common/git
+    ./../modules/common/zen
+    ./../modules/common/sops
+    ./../modules/common/vicinae
+    ./../modules/common/radicle
+    ./../modules/common/ghostty
+    ./../modules/common/neru
+    ./../modules/nixos/hyprland
+  ];
+
+  git.enable = true;
+  radicle.enable = true;
+  sops.enable = true;
+  ghostty.enable = true;
+
+  home = {
+    packages = [
+      inputs.colmena.packages.${pkgs.stdenv.hostPlatform.system}.colmena
+      pkgs.sops
+      self.packages.${pkgs.stdenv.hostPlatform.system}.nvim
+    ];
+
+    sessionVariables = {
+      EDITOR = "nvim";
+      PAGER = "bat";
+    };
+    stateVersion = "25.05";
+  };
+  programs.home-manager.enable = true;
+}
