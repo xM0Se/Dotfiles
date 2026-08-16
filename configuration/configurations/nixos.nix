bea5f63186c48d7dcabb9ccefc4a37ffc589fd81
xM0Se <git@xM0Se.dev>
2026-08-16 11:43:44 +0200

    feat(configuration/configuration): updated nixos config

--- configuration/configurations/nixos.nix
+++ configuration/configurations/nixos.nix
@@ -1,1 +1,25 @@
-{}
+{
+  inputs,
+  lib,
+  ...
+}: {
+  imports = [
+    inputs.sops-nix.nixosModules.sops
+    inputs.home-manager.nixosModules.home-manager
+    ../modules/nixos/ssh
+    ../modules/common/sops.nix
+    ../modules/common/home-manager.nix
+    ../modules/common/common.nix
+  ];
+
+  custom = {
+    home-manager.enable =
+      lib.mkDefault true;
+
+    ssh.enable =
+      lib.mkDefault true;
+
+    sops.enable =
+      lib.mkDefault true;
+  };
+}
