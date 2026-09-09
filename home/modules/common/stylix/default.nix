4b90dba44059e32921772ab253ad59a6abfa95ef
xM0Se <git@xM0Se.dev>
2026-09-09 21:35:00 +0200

    feat(home/modules/common): TESTING Stylix

--- home/modules/common/stylix/default.nix
+++ home/modules/common/stylix/default.nix
@@ -1,2 +1,38 @@
 {
+  inputs,
+  pkgs,
+  ...
+}: {
+  imports = [
+    inputs.stylix.homeModules.stylix
+  ];
+  stylix = {
+    enable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
+    # image = "";
+    cursor = {
+      package = pkgs.phinger-cursors;
+      name = "phinger-cursors-dark";
+      size = 32;
+    };
+    fonts = {
+      #   serif = {
+      #     package = pkgs.dejavu_fonts;
+      #     name = "DejaVu Serif";
+      #   };
+      #
+      #   sansSerif = {
+      #     package = pkgs.dejavu_fonts;
+      #     name = "DejaVu Sans";
+      #   };
+      monospace = {
+        package = pkgs.jetbrains-mono;
+        name = "JetBrains Mono";
+      };
+      emoji = {
+        package = pkgs.openmoji-color;
+        name = "OpenMoji Color";
+      };
+    };
+  };
 }
