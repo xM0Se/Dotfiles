0f3c9391b08e89b8c82fd9fedd1fca4172d70061
xM0Se <git@xM0Se.dev>
2026-09-09 21:31:24 +0200

    feat(home/modules/common): git: testing delta

--- home/modules/common/git/default.nix
+++ home/modules/common/git/default.nix
@@ -47,6 +47,20 @@
         };
       };
 
+      delta = {
+        enable = true;
+        enableGitIntegration = true;
+        options = {
+          decorations = {
+            commit-decoration-style = "bold yellow box ul";
+            file-decoration-style = "none";
+            file-style = "bold yellow ul";
+          };
+          features = "decorations";
+          whitespace-error-style = "22 reverse";
+        };
+      };
+
       gpg = {
         enable = true;
       };
